9cef2e57b8e315685d82a36a332b0fa00a896fca
jt <jt234wq@gmail.com>
2017-09-14 23:17:35 -0400

    index page setup

--- routes/polls.js
+++ routes/polls.js
@@ -5,23 +5,38 @@ const $ = require('jQuery')(window);
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+router.post('/createpoll', function (req, res, next) {
   const db = require('../db/database.js');
-  const text = 'INSERT INTO poll(poll_name) VALUES($1)';
-  const values = ["wtf"];
-
-  res.render('index');
-  $(".test").click(function () {
-    console.log("Handler for .click() called.");
-    db.query(text, values, (err, result) => {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        console.log("success");
-      }
-    })
-  });
+  const pollText = 'INSERT INTO poll(poll_name) VALUES($1)';
+  const pollOptionText = 'INSERT INTO poll_option(option) VALUES';
+  const values = [req.body.title];
+  var optionValues = [];
+  console.log(req.body);
+  db.query(pollText, values, (err, result) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log("success");
+    }
+  })
+  Object.keys(req.body).forEach(function (key, index) {
+    // key: the name of the object key
+    // index: the ordinal position of the key within the object 
+    console.log(key, index)
+    if (index > 0) {
+      optionValues.push(req.body[key])
+      console.log(req.body[key])
+    }
+  })
+  console.log(optionValues)
+  db.query(pollOptionText, optionValues, (err, result) => {
+    if (err) {
+      console.log("sql", err.stack);
+    } else {
+      console.log("success");
+    }
+    res.send("complete");
+  })
 });
 
 module.exports = router;
