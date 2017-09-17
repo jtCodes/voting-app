0a122e9e8a83ed41285f7d3dcccd709ad43563f1
J Tan <jt234wq@gmail.com>
2017-09-17 13:32:01 -0400

    chart fix

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,37 +1,85 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var randomScalingFactor = function () {
+    return Math.round(Math.random() * 100);
+};
+
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+var config = {
+    type: 'pie',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+            ],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(54, 162, 235, 0.2)',
             ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+            label: 'Dataset 1'
+        }],
+        labels: [
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue"
+        ]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+        responsive: true
     }
+};
+
+window.onload = function () {
+    var ctx = document.getElementById("chart-area").getContext("2d");
+    window.myPie = new Chart(ctx, config);
+};
+
+document.getElementById('randomizeData').addEventListener('click', function () {
+    config.data.datasets.forEach(function (dataset) {
+        dataset.data = dataset.data.map(function () {
+            return randomScalingFactor();
+        });
+    });
+
+    window.myPie.update();
+});
+
+var colorNames = Object.keys(window.chartColors);
+document.getElementById('addDataset').addEventListener('click', function () {
+    var newDataset = {
+        backgroundColor: [],
+        data: [],
+        label: 'New dataset ' + config.data.datasets.length,
+    };
+
+    for (var index = 0; index < config.data.labels.length; ++index) {
+        newDataset.data.push(randomScalingFactor());
+
+        var colorName = colorNames[index % colorNames.length];;
+        var newColor = window.chartColors[colorName];
+        newDataset.backgroundColor.push(newColor);
+    }
+
+    config.data.datasets.push(newDataset);
+    window.myPie.update();
+});
+
+document.getElementById('removeDataset').addEventListener('click', function () {
+    config.data.datasets.splice(0, 1);
+    window.myPie.update();
 });
